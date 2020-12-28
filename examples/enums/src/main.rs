575744a98d23ef1619b6e9727e584c1045cbf6e9
samuel <samuel@samuels-MacBook-Pro.local>
2020-12-28 00:09:18 +0000

    removing debug folder from repo

--- examples/enums/src/main.rs
+++ examples/enums/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-        V4(String),
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
         V6(String),
-    }
+     }
 
     // struct IpAddr {
     //     kind: IpAddrKind,
@@ -19,8 +20,6 @@ fn main() {
     //     address: String::from("::1"),
     // };
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
-
-
 }
