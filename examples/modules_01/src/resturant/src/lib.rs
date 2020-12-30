f407af1f90561361c6d242cfe34cd328ac88de1e
samuel <samuel@samuels-MacBook-Pro.local>
2020-12-30 15:17:17 +0000

    Making stucts and enumbs public

--- examples/modules_01/src/resturant/src/lib.rs
+++ examples/modules_01/src/resturant/src/lib.rs
@@ -5,6 +5,20 @@ mod front_of_house {
 }
  
 mod back_of_house {
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order(){
         cook_order();
         super::serve_order();
@@ -13,12 +27,23 @@ mod back_of_house {
 
 }
 pub fn eat_at_resturaurant() {
+    // Order breakfast with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change what bread we want
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // This next line wont compile as we're not allowed to see
+    // or modify the fruits
+    // meal.seasonal_fruit = String::from("blueberries");
+
+
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
 }
-
 
 fn serve_order(){}
