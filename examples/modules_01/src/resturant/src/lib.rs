954ad9f34ed09e34ef0d91c74c4e69ec6de7d1be
samuel <samuel@samuels-MacBook-Pro.local>
2020-12-30 14:31:53 +0000

    'super::' can be used to access functions outside of the module, while functions inside can be called normally

--- examples/modules_01/src/resturant/src/lib.rs
+++ examples/modules_01/src/resturant/src/lib.rs
@@ -3,7 +3,15 @@ mod front_of_house {
         pub fn add_to_waitlist(){}
     }
 }
+ 
+mod back_of_house {
+    fn fix_incorrect_order(){
+        cook_order();
+        super::serve_order();
+    }
+    fn cook_order(){}
 
+}
 pub fn eat_at_resturaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
@@ -11,3 +19,6 @@ pub fn eat_at_resturaurant() {
     // Relative path
     front_of_house::hosting::add_to_waitlist();
 }
+
+
+fn serve_order(){}
