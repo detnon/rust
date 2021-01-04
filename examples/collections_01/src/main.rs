7439de6a8ba406b826648f850f25525685ebc4af
detnon <samueldetnon@gmail.com>
2021-01-04 22:53:18 +0000

    creating vectors an adding information

--- examples/collections_01/src/main.rs
+++ examples/collections_01/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
-}
+// Vectors can be created empty with the type annotated
+let v: Vec<i32> = Vec::new(); 
+
+// The type can also be inferred by rust when using the `vec!` macro
+let mut v = vec![1, 2, 3];
+
+// New data can be pushed to a vector like so:
+
+v.push(4);
+v.push(5);
+v.push(6);
+
+// Vectors can only contain one type
+} // V will go out of scope after this point
