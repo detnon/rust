dbed7f1c787c7ceb51298b796f1e54cce7ebf400
detnon <samueldetnon@gmail.com>
2021-01-04 23:50:57 +0000

    Accessing elents of a vector

--- examples/collections_01/src/main.rs
+++ examples/collections_01/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
 let v: Vec<i32> = Vec::new(); 
 
 // The type can also be inferred by rust when using the `vec!` macro
+// Vectors can only contain one type
+
 let mut v = vec![1, 2, 3];
 
 // New data can be pushed to a vector like so:
@@ -11,5 +13,15 @@ v.push(4);
 v.push(5);
 v.push(6);
 
-// Vectors can only contain one type
+let nv = vec![1,2,3,4,5];
+
+let third: &i32 = &nv[2];
+println!("the third element is {}", third);
+
+match nv.get(2) {
+    Some(third) => println!("The third element is {}", third),
+    None => println!("There is no third element."),
+}
+
+
 } // V will go out of scope after this point
