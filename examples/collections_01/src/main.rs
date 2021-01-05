d2753a43a638bf4ed8c08e76e0b8228807a3bbd7
detnon <samueldetnon@gmail.com>
2021-01-05 00:15:05 +0000

    iterating over vectors and changing the stored values

--- examples/collections_01/src/main.rs
+++ examples/collections_01/src/main.rs
@@ -23,5 +23,20 @@ match nv.get(2) {
     None => println!("There is no third element."),
 }
 
+// Iterate over Vectors
+let vi = vec![100, 32, 57];
+
+for i in &vi {
+    println!("{}", i);
+}
+
+// When iterating over this vector it must be `mut`
+// in order to change the values
+let mut vm = vec![42, 26, 9];
+for i in &mut vm {
+    // `*` dereferences `i` in order to change its value
+    *i += 50;
+    println!("{}", i);
+}
 
 } // V will go out of scope after this point
